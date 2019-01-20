47e35b8c3ab0c01f517dab9d50c08d8334f26c26
Aron Gida <ifAronisGida@github.com>
2019-01-20 11:58:59 +0100

    added static methods and list to hold shopping cart

---
+++ src/main/java/com/codecool/servlet/ShoppingCartServlet.java
@@ -0,0 +1,29 @@
+package com.codecool.servlet;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import java.util.ArrayList;
+import java.util.List;
+
+@WebServlet (name = "Shopping Cart", urlPatterns = {"/shoppingCart"})
+public class ShoppingCartServlet extends HttpServlet {
+
+    public static List<Item> shoppingCartList = new ArrayList<>();
+
+    public static void addToCart(int id) {
+        int listSize = shoppingCartList.size();
+        for (Item item: ItemStore.itemList) {
+            if (item.getId() == id) {
+                shoppingCartList.add(listSize, item);
+            }
+        }
+    }
+
+    public static void removeFromCart(int id) {
+        for (Item item: ItemStore.itemList) {
+            if (item.getId() == id) {
+                shoppingCartList.remove(item);
+            }
+        }
+    }
+}
